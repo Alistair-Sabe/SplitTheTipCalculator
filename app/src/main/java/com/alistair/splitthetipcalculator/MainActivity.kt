fde2faa31f6979839ae123e7a8cf3f89c92e57e6
Alistair Sabe <alistairsabe96@gmail.com>
2023-02-13 13:27:06 +0200

    refactor inputField compose function

--- app/src/main/java/com/alistair/splitthetipcalculator/MainActivity.kt
+++ app/src/main/java/com/alistair/splitthetipcalculator/MainActivity.kt
@@ -1,37 +1,46 @@
 package com.alistair.splitthetipcalculator
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.alistair.splitthetipcalculator.components.InputField
 import com.alistair.splitthetipcalculator.ui.theme.SplitTheTipCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            myApp {
-                TopHeader()
+            MyApp {
+                //TopHeader()
+                MainContent()
             }
     
         }
     }
 }
 @Composable
-fun myApp(content: @Composable () -> Unit) {
+fun MyApp(content: @Composable () -> Unit) {
 
     SplitTheTipCalculatorTheme {
     // A surface container using the 'background' color from the theme
@@ -42,7 +51,6 @@ fun myApp(content: @Composable () -> Unit) {
     }
 }
 
-@Preview
 @Composable
 fun TopHeader(totalPerPerson: Double = 100.0) {
     Surface(modifier = Modifier
@@ -69,18 +77,56 @@ fun TopHeader(totalPerPerson: Double = 100.0) {
     
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MainContent(){
+    BillForm() { billAmt ->
+        Log.d("AMT", "MainContent: $billAmt"
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SplitTheTipCalculatorTheme {
-        myApp {
-            Text("Tip Calculator")
-        }
-
+        )
     }
 }
+
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BillForm(modifier: Modifier = Modifier,
+                onValChange: (String) -> Unit = {})
+    {
+        val totalBillState = remember {
+            mutableStateOf("")
+        }
+        val validState = remember(totalBillState.value) {
+            totalBillState.value.trim().isNotEmpty()
+        }
+        val keyboardController = LocalSoftwareKeyboardController.current
+
+        Surface(
+            modifier = Modifier
+                .padding(2.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+            border = BorderStroke(width = 1.dp, Color.LightGray)
+
+        ) {
+            Column() {
+                InputField(valueState = totalBillState,
+                    labelId = "Enter Bill Total",
+                    enabled = true,
+                    isSingleLine = true,
+                    onAction = KeyboardActions {
+                        if (!validState) return@KeyboardActions
+                        //Todo - onValueChanged
+                        onValChange(totalBillState.value.trim())
+
+                        keyboardController?.hide()
+
+                    })
+
+
+            }
+
+        }
+    }
+
