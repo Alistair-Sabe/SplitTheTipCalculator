6456575cc1783f2abe57899830c09d25164499eb
Alistair Sabe <alistairsabe96@gmail.com>
2023-02-13 10:56:45 +0200

    Design header section of app

--- app/src/main/java/com/alistair/splitthetipcalculator/MainActivity.kt
+++ app/src/main/java/com/alistair/splitthetipcalculator/MainActivity.kt
@@ -3,30 +3,70 @@ package com.alistair.splitthetipcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.alistair.splitthetipcalculator.ui.theme.SplitTheTipCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SplitTheTipCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            myApp {
+                TopHeader()
             }
+    
         }
     }
+}
+@Composable
+fun myApp(content: @Composable () -> Unit) {
+
+    SplitTheTipCalculatorTheme {
+    // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TopHeader(totalPerPerson: Double = 100.0) {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+
+        color = Color(0xFF90EE90)
+        ) {
+        Column(modifier = Modifier.padding(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center){
+            //formatting the parsed value
+            val total = "%.2f".format(totalPerPerson)
+            Text(text = "Total Per Person",
+                style = MaterialTheme.typography.h5)
+            Text(text = "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.Bold)
+
+        }
+        
+    }
+    
 }
 
 @Composable
@@ -38,6 +78,9 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     SplitTheTipCalculatorTheme {
-        Greeting("Android")
+        myApp {
+            Text("Tip Calculator")
+        }
+
     }
 }
